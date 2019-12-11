c90bd7df881dbafe831f8382d1dc2356002d0a29
BKinya <gitongakinya1@gmail.com>
2019-12-11 08:41:41 +0300

    Login callback

--- app/src/main/java/com/beatrice/bookfinder/LoginFragment.java
+++ app/src/main/java/com/beatrice/bookfinder/LoginFragment.java
@@ -4,7 +4,9 @@ package com.beatrice.bookfinder;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,9 +30,21 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_login, container, false);
-        EditText usernameEditTxtview = rootView.findViewById(R.id.username_edittxt);
-        EditText passwordEditTxtview = rootView.findViewById(R.id.password_editxt);
+        final EditText usernameEditTxtview = rootView.findViewById(R.id.username_edittxt);
+        final EditText passwordEditTxtview = rootView.findViewById(R.id.password_editxt);
         Button loginBtn = rootView.findViewById(R.id.login_btn);
+
+        //add a callback
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditTxtview.getText().toString();
+                String password = passwordEditTxtview.getText().toString();
+                if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password)){
+                    Navigation.findNavController(v).navigate(R.id.action_loginFragment_to_welcomeFragment);
+                }
+            }
+        });
 
         return rootView;
     }
